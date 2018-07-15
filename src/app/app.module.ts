c5687a5d1f22121c3a70b3aad08b267b79c5d380
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-07-14 19:37:01 -0700

    Created camping-site-detail component, implemented routing, implemented template and stylesheet for this component, added testing images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,23 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import {routing} from './app.rounting';
 import {CampingSiteGridComponent} from './camping-site-grid/camping-site-grid.component';
+import { CampingSiteDetailComponent } from './camping-site-detail/camping-site-detail.component';
+
+// third part imports
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    CampingSiteGridComponent
+    CampingSiteGridComponent,
+    CampingSiteDetailComponent
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    CarouselModule.forRoot()  // ngx-bootstrap
   ],
   providers: [],
   bootstrap: [AppComponent]
