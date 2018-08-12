3888585a12083079c47053f9bad919a8cf35ff6c
xiaodongm <39108568+xiaodongm@users.noreply.github.com>
2018-08-11 23:37:02 -0700

    updated stylings for event list page.

--- src/app/services/map.service.client.ts
+++ src/app/services/map.service.client.ts
@@ -1,7 +1,7 @@
 export class MapServiceClient {
 
   reverseGeocoding(lat, lng) {
-    return fetch('https://nominatim.openstreetmap.org/reverse?format=json&lat=' + lat + '&lon=' + lng, {
+    return fetch('https://nominatim.openstreetmap.org/reverse?format=json&lat=' + lat + '&lon=' + lng + '&accept-language=en', {
       method: 'GET',
       mode: 'cors',
     })
